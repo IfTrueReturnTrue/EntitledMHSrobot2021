346f2f805d3149373b8dca9bfa95f29e9c6bad05
mykales <han0029@mhs.vic.edu.au>
2021-05-18 21:56:02 +1000

    Update attack(modifiedforsim).py

--- attack(modifiedforsim).py
+++ attack(modifiedforsim).py
@@ -1,6 +1,3 @@
-#!/usr/bin/eom ev3sim-micropython
-
-
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor
@@ -9,13 +6,15 @@ from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor
 
 
 import time
-
+import threading
 
 
 
 
 #Commence parts testing:
 #define functions here
+#APPEND: Assuming motor numbers based on clockwise position starting from true north 0^.
+#APPEND: Assuming North and south motors are facing east. East and west motors are facing north.
 motor1 = MediumMotor(OUTPUT_A)
 motor2 = MediumMotor(OUTPUT_B)
 motor3 = MediumMotor(OUTPUT_C)
@@ -26,67 +25,83 @@ motor4 = MediumMotor(OUTPUT_D)
 ultrasonicFront = UltrasonicSensor(INPUT_2)
 ultrasonicLeft  = UltrasonicSensor(INPUT_3)
     #initialise motors (omni wheels)
-    
-#SPEED between 0, 100
-def moveatspeedindir(s, dir):
+
+#S = SECONDS
+#K = SPEED MULTIPLIER (-1 or 1)
+#this function runs individual motors. Note that north and south motors are facing east, etc... Refer to #append at function definitions
+def runmotor(s, k, motor):
     try:
-        rot = 1
-        if dir == "left":
-            motor3.on_for_rotations(s,  rot)#here rot is only a placeholder.
-            motor4.on_for_rotations(s,  rot)
-        elif dir == "right":
-            motor3.on_for_rotations(s,  -rot)  #negative rot for opposite to normal
-            motor4.on_for_rotations(s,  -rot)  
-        elif dir == "up":
-            motor1.on_for_rotations(s,  rot)
-            motor2.on_for_rotations(s,  rot)
-        elif dir == "down":
-            motor1.on_for_rotations(s,  -rot)  
-            motor2.on_for_rotations(s,  -rot)
+        if motor == "north":
+            motor1.on_for_seconds(100 * k, s)
+        elif motor == "south":
+            motor3.on_for_seconds(100 * k, s)
+        elif motor == "east":
+            motor2.on_for_seconds(100 * k, s)
+        elif motor == "west":
+            motor4.on_for_seconds(100 * k, s)
         else:
             pass
-            
-        
+
+
     except:
         print("Error, something happened.")
         pass
+
+#this function moves the entire bot in one cardinal direction. Threading allows functions to run parallel to main code.
+def movedir(s, direction):
+    try:
+        if direction == "north":
+            threading.Thread(target=runmotor, args=(1, 1, "east")).start()
+            threading.Thread(target=runmotor, args=(1, 1, "west")).start()
+        elif direction == "south":
+            threading.Thread(target=runmotor, args=(1, -1, "east")).start()
+            threading.Thread(target=runmotor, args=(1, -1, "west")).start()
+        elif direction == "east":
+            threading.Thread(target=runmotor, args=(1, 1, "north")).start()
+            threading.Thread(target=runmotor, args=(1, 1, "south")).start()
+        elif direction == "west":
+            threading.Thread(target=runmotor, args=(1, -1, "north")).start()
+            threading.Thread(target=runmotor, args=(1, -1, "south")).start()
+        else:
+            pass
+
+
+    except:
+        print("Error, something happened.")
+        pass
+
 
 def sqrt(num):
     return num**0.5
 somevalue = 0
 def ultraSensingForMovement():
 
-    errorAmount =3
+    errorAmount = 0
     forwardFB=ultrasonicFront.distance_centimeters
     sideFB=ultrasonicLeft.distance_centimeters
-    oppCalc1 = sideFB/2
-    oppCalc2 = forwardFB*(3/(sqrt(3)))
-    if oppCalc1 - oppCalc2 < abs(errorAmount):
-        
+    calc1 = 1/sqrt(2)*sideFB #JUST SOME RECALCULATIONS TO THE CODE
+    if (calc1 >= forwardFB - errorAmount) and calc1 <= forwardFB + errorAmount:
         print("There is a wall")
         return False
     else:
-        
-        print("There is a ball.")
+        print("There is a ball")
         return True
+    '''oppCalc1 = sideFB/2
+    oppCalc2 = forwardFB*(3/(sqrt(3)))
+    if oppCalc1 - oppCalc2 < abs(errorAmount):
 
+        print("There is a wall")
+        return False
+    else:
 
-    
-    
+        print("There is a ball.")
+        return True'''
 
-
-
-
-
-print("yes") #test if everything is working (no syntatical errors)
-
-ultraSensingForMovement() # initial test
-if ultraSensingForMovement():
-    #5 is just an arbritrary number below. 
-    for x in range(5): #This just repeats the movement 5 times
-        moveatspeedindir(100, "right")
-time.sleep(1)
-
-
-
-    
+while(True):
+    print("start")
+    ultraSensingForMovement()
+    movedir(1, "north") #keeping to one second to match with time sleep, so stacking does not occur
+    movedir(1, "east") #can edit this in if functions to match with ball sense.
+                       #Though would not recommend as ball sense is still highly innacurate
+    print("end")
+    time.sleep(1)
